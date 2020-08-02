935e837573b980d61e6c57885958e0b38d7f76ae
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-08-02 19:41:27 +0530

    add and design wheel

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -5,6 +5,26 @@ class Wheel extends React.Component {
     render() {
         return (
             <div className="wheel-container">
+                <div className="wheel" id="wheel" >
+                    <div className="controls" id="menu-btn">
+                        <div>MENU</div>
+                    </div>
+                    <div className="controls" id="forward-btn">
+                        <i className="fas fa-fast-forward"></i>
+                    </div>
+                    <div className="controls" id="play-pause-btn">
+                        <div>
+                            <i className="fas fa-play"></i>
+                            <i className="fas fa-pause"></i>
+                        </div>
+                    </div>
+                    <div className="controls" id="reverse-btn">
+                        <i className="fas fa-fast-backward"></i>
+                    </div>
+                </div>
+
+                <div className="centre-btn" id="centre-btn"></div>
+
                
             </div>
         )
