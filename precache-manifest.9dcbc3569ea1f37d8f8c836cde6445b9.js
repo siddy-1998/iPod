fa570e26ff6838158404dc62458632ab983a39da
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-08-05 22:50:55 +0530

    Updates

---
+++ precache-manifest.9dcbc3569ea1f37d8f8c836cde6445b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6a1f55148c73b9de11384b5d4dcfc3",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "c94fa157a13b9079f00b",
+    "url": "/iPod/static/css/main.ddbe6263.chunk.css"
+  },
+  {
+    "revision": "2407266f886ba7b3a479",
+    "url": "/iPod/static/js/2.65b37f0b.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod/static/js/2.65b37f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94fa157a13b9079f00b",
+    "url": "/iPod/static/js/main.a9979cb2.chunk.js"
+  },
+  {
+    "revision": "133d1a464549fda180f0",
+    "url": "/iPod/static/js/runtime-main.eb9bf7dd.js"
+  },
+  {
+    "revision": "a08b7f040ac4fa956db33526b9d6e3dc",
+    "url": "/iPod/static/media/Attention.a08b7f04.mp3"
+  },
+  {
+    "revision": "8453083e74f4524cab66c097b91e9d1c",
+    "url": "/iPod/static/media/Bruises.8453083e.mp3"
+  },
+  {
+    "revision": "9a23e82d7d411ce9e4acb076e0079915",
+    "url": "/iPod/static/media/Falling.9a23e82d.mp3"
+  },
+  {
+    "revision": "6bf32257f6c669894af346c32f52dadc",
+    "url": "/iPod/static/media/attentionCI.6bf32257.png"
+  },
+  {
+    "revision": "ac53252056b596b05df72ce19c7b9429",
+    "url": "/iPod/static/media/bruisesCI.ac532520.jpg"
+  },
+  {
+    "revision": "43e315a74f5db84befb0bd17e25cef40",
+    "url": "/iPod/static/media/fallingCI.43e315a7.jpg"
+  },
+  {
+    "revision": "6a16064f608babf03c761fdc620ed258",
+    "url": "/iPod/static/media/game.6a16064f.jpg"
+  },
+  {
+    "revision": "03ca06ba1c1c23c19c52351347511fd4",
+    "url": "/iPod/static/media/music.03ca06ba.png"
+  },
+  {
+    "revision": "594ba13187edefd9182d63dd4ec6c8b3",
+    "url": "/iPod/static/media/settings.594ba131.jpg"
+  }
+]);
